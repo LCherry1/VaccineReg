663a1b3d2576c0d473f39521f8ae321729525b07
LCherry1 <logancherry01@gmail.com>
2021-08-18 10:50:17 +0200

    commit clean code

--- UserVaccine.py
+++ UserVaccine.py
@@ -4,18 +4,18 @@ FirstName = input("Firstname: ")
 gender = input("Gender:")
 Age = input("Age: ")
 province = input("Province : ")
-#1 st Branch
+
 
 
 
 if(Age < 25 ):
     print("You are not applicable for the Covid 19 vaccine")
-    #Comment
+   
 
 else:
     print("You quailify for the vaccine")
     #Test to see if changes are mad
-    #oivdl;mgpeombpoermeplmb
+
     
 
 
