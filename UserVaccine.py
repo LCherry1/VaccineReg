51e76c90a0ba611d2b5ef1faf0e7d866d40d3918
LCherry1 <88708783+LCherry1@users.noreply.github.com>
2021-08-18 09:59:29 +0200

    Add files via upload

---
+++ UserVaccine.py
@@ -0,0 +1,15 @@
+id = input("ID: ")
+SurName = input("Surname: ")
+FirstName = input("Firstname: ")
+gender = input("Gender:")
+Age = input("Age: ")
+province = input("Province : ")
+
+
+if(Age < 25 ):
+    print("You are not applicable for the Covid 19 vaccine")
+
+else:
+    print("You quailify for the vaccine")
+
+
